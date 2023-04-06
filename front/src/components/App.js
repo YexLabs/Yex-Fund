e8d5861d86ff91380bdd37280639924d2e0a9779
0xXiaoChen <1255552127@qq.com>
2023-04-06 21:27:17 +0800

    update app.js

---
+++ front/src/components/App.js
@@ -0,0 +1,68 @@
+import { WagmiConfig, createClient } from "wagmi";
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultClient,
+} from "connectkit";
+import { mainnet, goerli } from "@wagmi/core";
+import { Outlet, useNavigate } from "react-router-dom";
+import logo from "../logo.svg";
+import "./App.css";
+
+const alchemyId = process.env.ALCHEMY_ID;
+
+const chains = [goerli, mainnet];
+
+const client = createClient(
+  getDefaultClient({
+    appName: "GLD DeFi",
+    alchemyId,
+    chains,
+  })
+);
+
+const App = () => {
+  const navigate = useNavigate();
+  const BuySell = () => {
+    navigate("/Switch");
+  };
+  const ExchangeClick = () => {
+    navigate("/Exchange");
+  };
+
+  return (
+    <WagmiConfig client={client}>
+      <div id="Top-line" className="flex justify-between h-16">
+        <div className="flex flex-row">
+          <div className="ml-2 flex justify-center flex-col">
+            <img className="h-8" src={logo} />
+          </div>
+          <div className="justify-center flex flex-col ml-1 text-white">
+            <div
+              className="btn btn-sm btn-ghost text-xl"
+              onClick={() => BuySell()}
+            >
+              Switch
+            </div>
+          </div>
+          <div className="justify-center flex flex-col ml-1 text-white">
+            <div
+              className="btn btn-sm btn-ghost text-xl"
+              onClick={() => ExchangeClick()}
+            >
+              Exchange
+            </div>
+          </div>
+        </div>
+        <div className="mr-5 justify-center flex flex-col">
+          <ConnectKitProvider>
+            <ConnectKitButton />
+          </ConnectKitProvider>
+        </div>
+      </div>
+      <Outlet />
+    </WagmiConfig>
+  );
+};
+
+export default App;
