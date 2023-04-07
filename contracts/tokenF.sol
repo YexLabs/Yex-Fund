8d8dab4a2eba3ec505f7b5327a264af8ab7140b0
KandHisC <shichuan@ualberta.ca>
2023-04-07 21:44:49 +0800

    formatting the code

--- contracts/tokenF.sol
+++ contracts/tokenF.sol
@@ -1,22 +1,21 @@
-
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // SPDX-License-Identifier: GPL-3.0
-contract tokenF is ERC20{
+contract tokenF is ERC20 {
     uint256 public TotalSupply;
     address dev;
-    constructor(uint256 initialSupply,address _dev) ERC20("Gold", "GLD") {
+
+    constructor(uint256 initialSupply, address _dev) ERC20("Gold", "GLD") {
         TotalSupply = initialSupply;
-        dev=_dev;
+        dev = _dev;
     }
 
     uint256 public CurrentAmount = 0;
 
     mapping(address => bool) public AccessJudgeForMint;
-
 
     function mint(address _account, uint256 _amount) public {
         require(AccessJudgeForMint[msg.sender]);
@@ -26,14 +25,14 @@ contract tokenF is ERC20{
     }
 
     function burn(address _account, uint256 _amount) public {
-        require(AccessJudgeForMint[msg.sender]);      
+        require(AccessJudgeForMint[msg.sender]);
         require(CurrentAmount >= _amount);
         _burn(_account, _amount);
         CurrentAmount = CurrentAmount - _amount;
     }
 
-    function setMint(address _account) public{
-        require(msg.sender==dev,"Permission denied");
+    function setMint(address _account) public {
+        require(msg.sender == dev, "Permission denied");
         AccessJudgeForMint[_account] = true;
     }
 }
