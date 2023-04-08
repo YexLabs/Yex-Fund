f59394cbf6b58f31e1270bd874d4a63d4eff73e1
KandHisC <shichuan@ualberta.ca>
2023-04-08 13:20:21 +0800

    fix msg.sender error

--- contracts/core/OTC.sol
+++ contracts/core/OTC.sol
@@ -5,13 +5,16 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../interfaces/IOTC.sol";
 import "../interfaces/IVault.sol";
+import "../interfaces/IF.sol";
 
 contract OTC is IOTC {
     IVault private vault;
+    IF private f;
 
     constructor(IVault _vault) {
         // 先部署vault，再部署场外交易合约
         vault = _vault;
+        f = vault.f();
     }
 
     // 申购
@@ -29,7 +32,7 @@ contract OTC is IOTC {
         // TODO 检查是否允许赎回
         // 收款到F
         // vault.f.transferFrom(msg.sender, address(vault), _amount);
-        vault.f().burn(msg.sender, _amount);
+        f.burn(msg.sender, _amount);
         // TODO 计算价格
         // 发送代币
         vault.transferToken(token, msg.sender, (_amount * 99) / 100);
