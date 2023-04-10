376297a89b9f659a393bc3ab58fe703bba2d3caa
0xXiaoChen <1255552127@qq.com>
2023-04-11 02:18:48 +0800

    update google pdf

--- front/src/components/WhitePaper.js
+++ front/src/components/WhitePaper.js
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import pdf from "../whitepaper.pdf";
 
 export default function WhitePaper() {
   const [numPages, setNumPages] = useState(null);
@@ -10,10 +11,7 @@ export default function WhitePaper() {
   }
   return (
     <div className="mt-24 w-full items-center flex justify-center">
-      <Document
-        file={`${process.env.PUBLIC_URL}/whitepaper.pdf`}
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
+      <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
         {Array.from(new Array(numPages), (el, index) => (
           <Page key={`page_${index + 1}`} pageNumber={index + 1} />
         ))}
